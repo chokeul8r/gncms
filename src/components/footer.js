583ccaf5772d9f8d42744df2ccb6d46a713eaf21
Mark <mark.long@marklong.io>
2020-01-07 12:32:09 -0700

    Added React Bootstrap - Mod Header & Footer

---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Footer = () => (
+  <footer>
+    <div className="container">
+      <span className="text-muted">© {new Date().getFullYear()}</span>
+    </div>
+  </footer>
+)
+export default Footer
--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,24 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+// import { Link } from "gatsby"
+// import PropTypes from "prop-types"
+import {
+  Navbar,
+} from "react-bootstrap"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+const Header = props => (
+  <Navbar bg="transparent" expand="lg">
+    <div className="container" bg="transparent">
+      <Navbar.Brand href="#home">Logo</Navbar.Brand>
     </div>
-  </header>
+  </Navbar>
 )
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+// Header.propTypes = {
+//   siteTitle: PropTypes.string,
+// }
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
+// Header.defaultProps = {
+//   siteTitle: ``,
+// }
 
 export default Header
