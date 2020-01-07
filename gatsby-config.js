a4334d92b41f26c49f083ab62a6905e1abaf81b7
Mark <mark.long@marklong.io>
2020-01-07 00:28:41 -0700

    modified config source filesystem for data

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,21 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `uploads`,
+        path: `${__dirname}/static/assets`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/content`,
+      },
+    },
+    "gatsby-transformer-remark",
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
